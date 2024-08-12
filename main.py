3daeb536ec4c397a88d4698226bc066a6902dc9e
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-12 11:49:10 +0530

    speech recognition is done

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-
 import speech_recognition as sr
+import webbrowser
+import pyttsx3
+
+recognizer = sr.Recognizer()
+ttsx = pyttsx3.init()
+
+def speak(text) :
+    ttsx.say(text)
+    ttsx.runAndWait()
+
 
 if __name__ == "__main__" :
-    pass
+    speak("Good morning Pihu")
