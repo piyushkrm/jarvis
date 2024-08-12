b817ee3a9f4362e7aa81155f369c51e4ec4d2248
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-12 15:13:30 +0530

    Jarvis was running

--- main.py
+++ main.py
@@ -11,4 +11,22 @@ def speak(text) :
 
 
 if __name__ == "__main__" :
-    speak("Good morning Pihu")
+    speak("Initializing Jarvis....")
+
+    while True:
+
+        # Listen for the wake word "Jarvis"
+        # Obtain audio from the microphone
+
+        r = sr.Recognizer()
+        print("Recoginizing.......")
+
+        try:
+            with sr.Microphone() as source:
+                print("Listening......")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+        
+            command = r.recognize_google(audio)
+            print(command)
+        except Exception as e:
+            print("Error ; {0}".format(e))
