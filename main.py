a5adce5dd646986761efa2d34369b63b8603d4be
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-16 11:48:05 +0530

    jarvis working good

--- main.py
+++ main.py
@@ -10,8 +10,22 @@ def speak(text) :
     ttsx.runAndWait()
 
 
+# def processCommand(c) :
+#     if "open google" in c.lower() :
+#         webbrowser.open("https://google.com")
+        
+#     elif "open youtube" in c.lower() :
+#         webbrowser.open("https://youtube.com")
+
+#     elif "open facebook" in c.lower() :
+#         webbrowser.open("https://facebook.com")
+
+#     elif "open instagram" in c.lower() :
+#         webbrowser.open("https://instagram.com")
+
+
 if __name__ == "__main__" :
-    speak("Initializing Jarvis....")
+    speak("Hey Goodmorning, I am Alexa")
 
     while True:
 
@@ -19,14 +33,29 @@ if __name__ == "__main__" :
         # Obtain audio from the microphone
 
         r = sr.Recognizer()
-        print("Recoginizing.......")
+        print("Recoginizing")
+
+        with sr.Microphone() as source:
+            print("Listening")
+            audio = r.listen(source)
 
         try:
-            with sr.Microphone() as source:
-                print("Listening......")
-                audio = r.listen(source, timeout=2, phrase_time_limit=1)
-        
             command = r.recognize_google(audio)
             print(command)
+        
+            
+    #         word = r.recognize_google(audio)
+    #         if (word.lower() == "YOUTUBE") :
+    #             speak("Ya")
+
+    #             # Listen for command
+    #             with sr.Microphone() as source:
+    #                 print("Jarvis Active.....")
+    #                 audio = r.listen(source)
+    #                 command = r.recognize_google(audio)
+
+
+    #                 processCommand(command)
+
         except Exception as e:
             print("Error ; {0}".format(e))
