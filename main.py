8b0dca7d53c6f7c01ebc5b709a896b9ebfb3d9af
Piyush Mishra <143158860+piyushkrm@users.noreply.github.com>
2024-08-20 11:30:10 +0530

    importing music library

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
+import musicLibrary
+
 
 recognizer = sr.Recognizer()
 ttsx = pyttsx3.init()
@@ -10,22 +12,22 @@ def speak(text) :
     ttsx.runAndWait()
 
 
-# def processCommand(c) :
-#     if "open google" in c.lower() :
-#         webbrowser.open("https://google.com")
+def processCommand(c) :
+    if "open google" in c.lower() :
+        webbrowser.open("https://google.com")
         
-#     elif "open youtube" in c.lower() :
-#         webbrowser.open("https://youtube.com")
+    elif "open youtube" in c.lower() :
+        webbrowser.open("https://youtube.com")
 
-#     elif "open facebook" in c.lower() :
-#         webbrowser.open("https://facebook.com")
+    elif "open facebook" in c.lower() :
+        webbrowser.open("https://facebook.com")
 
-#     elif "open instagram" in c.lower() :
-#         webbrowser.open("https://instagram.com")
+    elif "open instagram" in c.lower() :
+        webbrowser.open("https://instagram.com")
 
 
 if __name__ == "__main__" :
-    speak("Hey Goodmorning, I am Alexa")
+    speak("Hey Goodmorning, I am Shree")
 
     while True:
 
@@ -35,27 +37,29 @@ if __name__ == "__main__" :
         r = sr.Recognizer()
         print("Recoginizing")
 
-        with sr.Microphone() as source:
-            print("Listening")
-            audio = r.listen(source)
 
         try:
+            with sr.Microphone() as source:
+                print("Listening")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)  
+                # , timeout=2, phrase_time_limit=1
+
             command = r.recognize_google(audio)
             print(command)
         
             
-    #         word = r.recognize_google(audio)
-    #         if (word.lower() == "YOUTUBE") :
-    #             speak("Ya")
+            word = r.recognize_google(audio)
+            if (word.lower() == "hey siri") :
+                speak("Yes Piyush, I am here to help you")
 
-    #             # Listen for command
-    #             with sr.Microphone() as source:
-    #                 print("Jarvis Active.....")
-    #                 audio = r.listen(source)
-    #                 command = r.recognize_google(audio)
+                # Listen for command
+                with sr.Microphone() as source:
+                    print("Shree Active")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
 
 
-    #                 processCommand(command)
+                    processCommand(command)
 
         except Exception as e:
-            print("Error ; {0}".format(e))
+            print("Error{0}".format(e))
